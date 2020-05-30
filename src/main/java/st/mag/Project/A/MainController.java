057125900ae90fabfd4dc290b51e41a7fa3937d3
georgi-uchkunov <56491370+georgi-uchkunov@users.noreply.github.com>
2020-05-30 14:38:02 +0300

    Update MainController.java

--- src/main/java/st/mag/Project/A/MainController.java
+++ src/main/java/st/mag/Project/A/MainController.java
@@ -1,9 +1,9 @@
 package st.mag.Project.A;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class MainController {
 
 	@GetMapping("/")
